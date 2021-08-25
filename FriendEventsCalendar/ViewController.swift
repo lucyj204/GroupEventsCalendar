cba5f380f193331af7e4ba45c8f10da6f0dc91fb
Lucy Joyce <lucyj20490@gmail.com>
2021-08-25 14:23:16 +0100

    Initial Commit

---
+++ FriendEventsCalendar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FriendEventsCalendar
+//
+//  Created by Lucy Joyce on 25/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
