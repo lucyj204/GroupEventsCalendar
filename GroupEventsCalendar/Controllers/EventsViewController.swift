0fb4582eb559fea565dcde9fd3d0808d907a3cb9
Lucy Joyce <lucyj20490@gmail.com>
2021-08-31 18:40:50 +0100

    Add bar button item to interface builder for EventsViewController and create textfield inside UIAlert to add new events to eventArray. Use NSPredicate to fetch and events relating to the selected group.

--- GroupEventsCalendar/Controllers/EventsViewController.swift
+++ GroupEventsCalendar/Controllers/EventsViewController.swift
@@ -10,12 +10,15 @@ import CoreData
 
 class EventsViewController: UITableViewController {
 
-    var groupArray = [Groups]()
+    var eventArray = [Events]()
     var selectedGroup : Groups? {
         didSet {
             tableView.reloadData()
+            loadEvents()
         }
     }
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     
     override func viewDidLoad() {
@@ -26,69 +29,72 @@ class EventsViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return eventArray.count
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let events = eventArray[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "EventNameCell", for: indexPath)
+        cell.textLabel?.text = events.event
+        
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+   
+//MARK: - Add New Event
+    
+    @IBAction func addButtonPressed(_ sender: Any) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Event", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Event", style: .default) { (action) in
+            let newEvent = Events(context: self.context)
+            newEvent.event = textField.text!
+            newEvent.parentGroup = self.selectedGroup
+            self.eventArray.append(newEvent)
+            self.saveEvent()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Add New Event"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    //MARK: - Model Manipulation Methods
+    
+    func saveEvent() {
+        do {
+            try context.save()
+        } catch {
+            print("Error saving event, \(error)")
+        }
+        tableView.reloadData()
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    
+    func loadEvents(with request: NSFetchRequest<Events> = Events.fetchRequest(), predicate: NSPredicate? = nil) {
+        let groupPredicate = NSPredicate(format: "parentGroup.name MATCHES %@", selectedGroup!.name!)
+        
+        if let additionalPredicate = predicate {
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [groupPredicate, additionalPredicate])
+        } else {
+            request.predicate = groupPredicate
+        }
+        
+        do {
+            eventArray = try context.fetch(request)
+        } catch {
+            print("Error fetching data from context")
+        }
+        tableView.reloadData()
+        
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
