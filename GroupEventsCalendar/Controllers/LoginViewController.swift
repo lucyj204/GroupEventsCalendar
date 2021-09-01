81d156a298d7301d6a1b6384a109e0ca570caa44
Lucy Joyce <lucyj20490@gmail.com>
2021-09-01 16:24:18 +0100

    Update register and login view controllers to authenticate users and segue to group view controller.

--- GroupEventsCalendar/Controllers/LoginViewController.swift
+++ GroupEventsCalendar/Controllers/LoginViewController.swift
@@ -7,7 +7,28 @@
 
 import Foundation
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
     
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+        if let email = emailTextField.text, let password = passwordTextField.text {
+            
+            Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    print(e)
+                } else {
+                    self.performSegue(withIdentifier: "goToGroups", sender: self)
+                }
+            }
+        }
+        
+    }
 }
+    
+
--- GroupEventsCalendar/Controllers/RegisterViewController.swift
+++ GroupEventsCalendar/Controllers/RegisterViewController.swift
@@ -7,7 +7,29 @@
 
 import Foundation
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
+    
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    @IBAction func registerButtonPressed(_ sender: UIButton) {
+        
+        if let email = emailTextField.text, let password = passwordTextField.text {
+            
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    print(e.localizedDescription)
+                } else {
+                    self.performSegue(withIdentifier: "goToGroups", sender: self)
+                }
+            }
+            
+        }
+        
+    }
     
 }
